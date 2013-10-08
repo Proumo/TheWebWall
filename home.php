00e65ca198043e48dffad5fdc7c3e0ff1e6b2489
alxferraz <andrelxferraz@gmail.com>
2013-10-07 22:22:25 -0300

    [André L. Ferraz]: alteraçoes para mostrar o conteudo do form do google

--- home.php
+++ home.php
@@ -46,7 +46,7 @@ $y=0;
             <div  id="faixaInferior" class="inferior well btn-custom"> 
 
             </div>
-            <div class="btn btn-info btn-lateral"><i class="icon-thumbs-up"></i> O que você está achando do site?</div>
+            <div id="feedback"class="btn btn-info btn-lateral"><i class="icon-thumbs-up"></i> O que você está achando do site?</div>
             <div id="postInfo" class=" postInfoFrame btn-custom">
                 <i class="icon-remove close" id="fechar"></i>
                 <div id="postInfoContent">             
