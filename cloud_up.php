cc2f3d5f166efa4e19a2b51bc4fe6bf9425cfde3
TonGarcia <ton_unb@hotmail.com>
2013-10-23 20:42:52 -0200

    [Ilton Garcia]: Arquivo cloud_up atualizado com os novos logins e senhas do Addon e também refatoração na localizacao da pasta de imagens.

--- cloud_up.php
+++ cloud_up.php
@@ -5,12 +5,12 @@
     require 'cloudinary/Api.php';
     
     \Cloudinary::config(array( 
-        "cloud_name" => "hef3kau58", 
-        "api_key" => "958848766151393", 
-        "api_secret" => "qEYfa-0sB0M4Gwn2KxxxNewImUM" 
+        "cloud_name" => "hxasx9ldn", 
+        "api_key" => "893558381195987", 
+        "api_secret" => "5O80V2hJMzCFodM9-utRc1KgJ4Y" 
     ));
     
-    $ref_img_up = \Cloudinary\Uploader::upload("/images/posts/261374180701.jpg");
+    $ref_img_up = \Cloudinary\Uploader::upload("images/posts/261374180701.jpg");
     
     var_dump($ref_img_up);
 
